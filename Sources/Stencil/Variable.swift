5ad8c414a29365f88bef66518a0fa9d304fd6c04
Grigory Entin <grigory.entin@gmail.com>
2023-04-24 01:43:52 +0200

    Tweaked.

--- Sources/Stencil/Variable.swift
+++ Sources/Stencil/Variable.swift
@@ -112,7 +112,7 @@ public struct Variable: Equatable, Resolvable {
       return resolve(bit: bit, collection: string)
     } else if let object = context as? NSObject {  // NSKeyValueCoding
       #if os(Linux)
-        return "XXX" // Mirror(reflecting: object).getValue(for: bit)
+        return Mirror(reflecting: context!).getValue(for: bit) ?? "No Mirror"
       #else
         if object.responds(to: Selector(bit)) {
           return object.value(forKey: bit)
